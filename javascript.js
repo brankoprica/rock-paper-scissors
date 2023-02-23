b6aac63c168f694fc35f42912322c0d3e7a4f268
Branko Prica <branko.prica@gmail.com>
2023-02-23 17:09:24 +0000

    added computer randomizer

---
+++ javascript.js
@@ -0,0 +1,10 @@
+console.log("hello world")
+
+const choices = ["rock", "paper", "scissors"]
+
+
+Math.floor((Math.random() *3)+1) -1
+
+let getComputerChoice = () => choices[(Math.floor((Math.random() *3)+1) -1)]
+
+console.log(getComputerChoice())
