b8aea875ebf03c20f50245daa12bd6334f3524d7
Branko Prica <branko.prica@gmail.com>
2023-02-23 22:22:12 +0000

    basically forgot to commit until now. it's done...

--- javascript.js
+++ javascript.js
@@ -1,10 +1,55 @@
-console.log("hello world")
-
 const choices = ["rock", "paper", "scissors"]
 
-
-Math.floor((Math.random() *3)+1) -1
-
 let getComputerChoice = () => choices[(Math.floor((Math.random() *3)+1) -1)]
+let computerChoice = getComputerChoice()
+yourScore=0
+computerScore=0
+ties=0
 
-console.log(getComputerChoice())
+let userChoice = ''
+let i=1
+let playRound = () => {   
+        getComputerChoice()
+
+        computerChoice=getComputerChoice()
+        if (i <=5){
+            if (computerChoice === userChoice){
+                console.log(`You both chose ${userChoice}! Its a tie!`)
+                ties ++
+            } else if (computerChoice === "rock" && userChoice === "scissors"){
+                console.log(`You chose ${userChoice} and your opponent chose ${computerChoice}. You lose!`)
+                computerScore ++
+            } else if (computerChoice === "rock" && userChoice === "paper") {
+                console.log(`You chose ${userChoice} and your opponent chose ${computerChoice}. You win!`)
+                yourScore ++
+            } else if (computerChoice === "paper" && userChoice === "rock") {
+                console.log(`You chose ${userChoice} and your opponent chose ${computerChoice}. You lose!`)
+                computerScore ++
+            } else if (computerChoice === "paper" && userChoice === "scissors") {
+                console.log(`You chose ${userChoice} and your opponent chose ${computerChoice}. You win!`)
+                yourScore ++
+            } else if (computerChoice === "scissors" && userChoice === "rock") {
+                console.log(`You chose ${userChoice} and your opponent chose ${computerChoice}. You win!`)
+                yourScore ++
+            } else if (computerChoice === "scissors" && userChoice === "paper") {
+                console.log(`You chose ${userChoice} and your opponent chose ${computerChoice}. You lose!`)
+                computerScore ++
+            } 
+            if (i <5){
+                console.log(`Your score: ${yourScore}. Your Opponent's score: ${computerScore}. Ties: ${ties}`)
+            }
+        }
+        if (i===5){
+            console.log(`Game over. Your score: ${yourScore}. Your Opponent's score: ${computerScore}. Ties: ${ties}`)
+            if (yourScore > computerScore) {
+                console.log(`You win!`)
+            } else if (yourScore < computerScore) {
+                console.log(`You lose!`)
+            } else {
+                console.log(`It's a tie!`)
+            }
+        }
+        if (i>5){
+            alert(`You've run out of tokens.`)
+        }
+}
